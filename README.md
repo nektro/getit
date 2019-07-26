e97116519386975e0c5754a202d90a6b16485eac
Meghan Denny <nektromc@gmail.com>
2019-07-26 19:21:44 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# GetIt
+![loc](https://tokei.rs/b1/github/nektro/getit)
+[![license](https://img.shields.io/github/license/nektro/getit.svg)](https://github.com/nektro/getit/blob/master/LICENSE)
+[![discord](https://img.shields.io/discord/551971034593755159.svg)](https://discord.gg/P6Y4zQC)
+
+Download files from an open directory into your local filesystem while preserving the folder structure
+
+[![buymeacoffee](https://www.buymeacoffee.com/assets/img/custom_images/orange_img.png)](https://www.buymeacoffee.com/nektro)
+
+## Getting Started
+
+### Installing
+```
+$ go get https://github.com/nektro/getit
+```
+
+### Usage
+```
+$ ~/go/bin/getit -url {URL}
+```
+
+> Note: GetIt will always save files relative to the current working directory. So running `getit -url https://example.com/path/to/dir/`, if it contains a link to `file.txt`, it will be saved to `./example.com/path/to/dir/file.txt`.
+
+## Built With
+- https://github.com/PuerkitoBio/goquery
+- https://github.com/deckarep/golang-set
+- https://github.com/nektro/go-util
+- https://github.com/schollz/progressbar
+
+## Contributing
+Issues and Pull Requests welcome!
+
+## License
+MIT. See [LICENSE](LICENSE) for more info.
+
+## Acknowledgments
+- The team behind `wget -m` and all of [GNU Wget](https://www.gnu.org/software/wget/)
+- [The Eye](https://the-eye.eu/) community
---
+++ main.go
@@ -0,0 +1,139 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+	mapset "github.com/deckarep/golang-set"
+	"github.com/nektro/go-util/util"
+	"github.com/schollz/progressbar"
+)
+
+var (
+	root  string
+	links = mapset.NewSet()
+)
+
+func main() {
+	val := flag.String("url", "", "The URL to search")
+	flag.Parse()
+
+	if len(*val) == 0 {
+		fmt.Println("-url is required!")
+		return
+	}
+
+	u, _ := url.Parse(*val)
+	u.Path = filepath.Dir(u.Path)
+	root = u.String()
+
+	fmt.Println("Input:", *val)
+	fmt.Println("Root: ", root)
+
+	crawlPage(*val)
+	fmt.Println()
+}
+
+func crawlPage(path string) {
+	fmt.Println("[DIR]", path)
+	//
+	res, err := http.Get(path)
+	if err != nil {
+		return
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		return
+	}
+
+	//
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		return
+	}
+
+	//
+	doc.Find("a").Each(func(i int, s *goquery.Selection) {
+		//
+		v, o := s.Attr("href")
+		if !o {
+			return
+		}
+		if strings.HasPrefix(v, "https://") || strings.HasPrefix(v, "http://") {
+			return
+		}
+		if strings.HasPrefix(v, "#") {
+			return
+		}
+		if strings.HasSuffix(v, "index.html") {
+			v = v[0 : len(v)-10]
+		}
+		if strings.HasSuffix(v, "index.htm") {
+			v = v[0 : len(v)-9]
+		}
+		n, _ := url.PathUnescape(v)
+		u, _ := url.Parse(path)
+		if strings.HasPrefix(n, "/") {
+			u.Path = n
+		} else {
+			u.Path = filepath.Join(u.Path, n)
+		}
+		p := u.String()
+
+		if !strings.HasPrefix(p, root) {
+			return
+		}
+		if !links.Add(p) {
+			return
+		}
+
+		if !hasFileExtension(p) {
+			crawlPage(p)
+			return
+		}
+		download(p)
+	})
+}
+
+func hasFileExtension(page string) bool {
+	a := strings.Split(page, "/")
+	b := a[len(a)-1]
+	return strings.Contains(b, ".")
+}
+
+const (
+	moveToStartOfLine = "\x1b[0G"
+)
+
+func download(path string) {
+	u, _ := url.Parse(path)
+	n, _ := url.PathUnescape(u.Path)
+	p, _ := filepath.Abs(u.Hostname() + n)
+	fmt.Println(p)
+
+	if util.DoesFileExist(p) {
+		return
+	}
+
+	res, _ := http.Get(path)
+	defer res.Body.Close()
+
+	d := filepath.Dir(p)
+	os.MkdirAll(d, os.ModePerm)
+	f, _ := os.OpenFile(p, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, os.ModePerm)
+	defer f.Close()
+
+	l := int(res.ContentLength)
+	bar := progressbar.NewOptions(l, progressbar.OptionSetBytes(l))
+	out := io.MultiWriter(f, bar)
+	io.Copy(out, res.Body)
+
+	fmt.Print(moveToStartOfLine)
+}
